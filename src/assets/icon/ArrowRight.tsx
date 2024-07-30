ab0ff57032260c3e47ece2ddff1b1c5285924ecd
Mathias Genibrel <mathias.geni@gmail.com>
2024-07-30 21:11:37 +0200

    feat: add download animation

---
+++ src/assets/icon/ArrowRight.tsx
@@ -0,0 +1,17 @@
+import { FC } from "react";
+
+export const ArrowRight: FC = () => {
+  return (
+    <svg
+      className="h-6 w-6 stroke-stone-800 stroke-2"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      aria-hidden="true"
+    >
+      <path d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75"></path>
+    </svg>
+  );
+};
--- src/header/components/DownloadLink.tsx
+++ src/header/components/DownloadLink.tsx
@@ -1,21 +1,77 @@
 import { FC } from "react";
 import clsx from "clsx";
 import { Apple } from "../../assets/icon/Apple.tsx";
+import { useIsHover } from "../../hooks/useIsHover.ts";
+import { motion, Transition } from "framer-motion";
+import { ArrowRight } from "../../assets/icon/ArrowRight.tsx";
 
 interface ButtonProps {}
 
+const transitionConfig: Transition = {
+  duration: 0.175,
+};
+
+const variants = {
+  leftIcon: {
+    initial: { opacity: 1, translateX: 0 },
+    animate: (isHover: boolean) =>
+      isHover ? { opacity: 0, translateX: -20 } : {},
+  },
+  text: {
+    initial: { translateX: 0 },
+    animate: (isHover: boolean) => (isHover ? { translateX: -20 } : {}),
+  },
+  rightIcon: {
+    initial: { opacity: 0, translateX: 10 },
+    animate: (isHover: boolean) =>
+      isHover ? { opacity: 1, translateX: -10 } : {},
+  },
+};
+
 export const DownloadLink: FC<ButtonProps> = () => {
+  const hover = useIsHover();
+
   return (
     <a
-      href={"#"}
+      onMouseEnter={hover.set}
+      onMouseLeave={hover.unset}
+      href={"/"}
       className={clsx(
-        "hidden items-center justify-center gap-2 overflow-hidden rounded-2xl bg-stone-800 bg-opacity-5 px-5 py-4 text-lg font-bold outline-none transition duration-300",
+        "relative hidden items-center justify-center gap-2 overflow-hidden rounded-2xl bg-stone-800 bg-opacity-5 px-5 py-4 text-lg font-bold outline-none",
         "focus:ring-2 focus:ring-purple-400",
         "sm:flex",
       )}
     >
-      <Apple />
-      Download for Mac
+      <motion.div
+        initial={"initial"}
+        animate={"animate"}
+        custom={hover.enabled}
+        variants={variants.leftIcon}
+        transition={transitionConfig}
+      >
+        <Apple />
+      </motion.div>
+
+      <motion.span
+        initial={"initial"}
+        animate={"animate"}
+        custom={hover.enabled}
+        variants={variants.text}
+        transition={transitionConfig}
+      >
+        Download for Mac
+      </motion.span>
+
+      <motion.div
+        className={"absolute right-0"}
+        initial={"initial"}
+        animate={"animate"}
+        custom={hover.enabled}
+        variants={variants.rightIcon}
+        transition={transitionConfig}
+      >
+        <ArrowRight />
+      </motion.div>
     </a>
   );
 };
